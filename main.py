1d93f605573342a5fdce51f091b5d60b5aea97aa
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2021-05-28 16:26:52 +1200

    Modified class+main for live video capture
     Not using triggering. Saves file.

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ cam.det_area_of_interest(y=100, height=300)
 
 cam.allocate_memory_for_image()
 
-# cam.activate_live_video()
+cam.activate_live_video()
 
-cam.triggered_video()
+cam.live_video_loop(savepath="dummy1.csv", start=0, stop=None)
+cam.live_video_loop(savepath="dummy2.csv", start=0, stop=None)
+# cam.activate_live_video(savepath='height_0_100.csv', start=0, stop=100)
+
 
 cam.close_connection()
 
--- uEye_class.py
+++ uEye_class.py
@@ -1,8 +1,37 @@
+import os
+os.environ["PYUEYE_DLL_PATH"] = r"C:\Program Files\IDS\uEye\USB driver package"
+
+from enum import IntEnum
+
 from pyueye import ueye
 import numpy as np
 import cv2
 import sys
 import ctypes
+import win32event
+
+from time import perf_counter_ns
+
+
+class IS_INIT_EVENT(ctypes.Structure):
+    _fields_ = [
+        ('nEvent', ueye.UINT),
+        ('bManualReset', ctypes.c_bool),
+        ('bInitialState', ctypes.c_bool),
+    ]
+
+
+class E_EVENT_CMD(IntEnum):
+    IS_EVENT_CMD_INIT    = 1
+    IS_EVENT_CMD_EXIT    = 2
+    IS_EVENT_CMD_ENABLE  = 3
+    IS_EVENT_CMD_DISABLE = 4
+    IS_EVENT_CMD_SET     = 5
+    IS_EVENT_CMD_RESET   = 6
+    IS_EVENT_CMD_WAIT    = 7
+
+
+is_Event = ueye._bind('is_Event', args=[ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p, ctypes.c_uint], returns=ueye.INT)
 
 
 class Camera(object):
@@ -14,12 +43,15 @@ class Camera(object):
         self.cInfo = ueye.CAMINFO()
         self.pcImageMemory = ueye.c_mem_p()
         self.MemID = ueye.int()
+        self.sequenceID = ueye.int()
         self.rectAOI = ueye.IS_RECT()
         self.pitch = ueye.INT()
         self.nBitsPerPixel = ueye.INT(8)  # 24: bits per pixel for color mode; take 8 bits per pixel for monochrome
         self.channels = 1  # 3 channels for color mode (RGB); take 1 channel for monochrome
         self.m_nColorMode = ueye.INT()  # Y8/RGB16/RGB24/REG32
         self.bytes_per_pixel = int(self.nBitsPerPixel / 8)
+        self.timestamps = []
+        self.edge_pos = []
 
     def configure(self):
         self.connect()
@@ -41,7 +73,8 @@ class Camera(object):
             print("Camera initialised")
 
     def get_data(self):
-        # Reads out the data hard-coded in the non-volatile camera memory and writes it to the data structure that cInfo points to
+        # Reads out the data hard-coded in the non-volatile camera memory and writes it to the data structure
+        # that cInfo points to
         nRet = ueye.is_GetCameraInfo(self.hCam, self.cInfo)
         if nRet != ueye.IS_SUCCESS:
             print("is_GetCameraInfo ERROR")
@@ -61,9 +94,9 @@ class Camera(object):
             print("Camera model:\t\t", self.sInfo.strSensorName.decode('utf-8'))
             print("Camera serial no.:\t", self.cInfo.SerNo.decode('utf-8'))
 
-        print(ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_EXTERNALTRIGGER))
-        print(ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_SUPPORTED_TRIGGER_MODE))
-        print(ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_TRIGGER_STATUS))
+        # print(ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_EXTERNALTRIGGER))
+        # print(ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_SUPPORTED_TRIGGER_MODE))
+        # print(ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_TRIGGER_STATUS))
 
     def reset_camera(self):
         nRet = ueye.is_ResetToDefault(self.hCam)
@@ -139,7 +172,7 @@ class Camera(object):
         print("Image height:\t", self.rectAOI.s32Height, height)
 
     def set_full_auto(self):
-        print("full auto")
+        print("Setting mode to full auto")
         disable = ueye.DOUBLE(0)
         enable = ueye.DOUBLE(1)
         zero = ueye.DOUBLE(0)
@@ -158,8 +191,10 @@ class Camera(object):
         print('EXP:',ret,ms)
 
     def allocate_memory_for_image(self):
-        # Allocates an image memory for an image having its dimensions defined by width and height and its color depth defined by nBitsPerPixel
-        nRet = ueye.is_AllocImageMem(self.hCam, self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel, self.pcImageMemory, self.MemID)
+        # Allocates an image memory for an image having its dimensions defined by width and height
+        # and its color depth defined by nBitsPerPixel
+        nRet = ueye.is_AllocImageMem(self.hCam, self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel,
+                                     self.pcImageMemory, self.MemID)
         if nRet != ueye.IS_SUCCESS:
             print("is_AllocImageMem ERROR")
         else:
@@ -179,7 +214,7 @@ class Camera(object):
             print("SetExternalTrigger ERROR")
         print('External trigger mode set', ueye.is_SetExternalTrigger(self.hCam, ueye.IS_GET_EXTERNALTRIGGER), trigmode)
 
-    def SetTriggerCounter(self, nValue):
+    def set_trigger_counter(self, nValue):
         return ueye.is_SetTriggerCounter(self.hCam, nValue)
 
     def capture_video(self, wait=False):
@@ -200,26 +235,22 @@ class Camera(object):
         wait_param = ueye.IS_WAIT if wait else ueye.IS_DONT_WAIT
         return ueye.is_FreezeVideo(self.hCam, wait_param)
 
-    def take_image(self):
+    def process_image(self, array, start=0, stop=None):
         # In order to display the image in an OpenCV window we need to...
-        # ...extract the data of our image memory
-        array = ueye.get_data(self.pcImageMemory, self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel,
-                              self.pitch, copy=False)
-        print('array',np.size(array))
-        # bytes_per_pixel = int(nBitsPerPixel / 8)
-
         # ...reshape it in an numpy array...
         frame = np.reshape(array, (self.rectAOI.s32Height.value, self.rectAOI.s32Width.value, self.bytes_per_pixel))
-
-        # ...resize the image by a half
+        # # ...resize the image by a half
         frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
-        # ---------------------------------------------------------------------------------------------------------------------------------------
-        # Include image data processing here: in this case, use Canny edge detection to find the edge
-        edges = cv2.Canny(frame, 0, 50)
-        # here's one value for the edge, at height '100' - we could also average over all horizontal slices.
-        print(np.argmax(edges[100]))
-        # ---------------------------------------------------------------------------------------------------------------------------------------
+        # # ------------------------------------------------------------------------------------------------------------
+        # Use Canny edge detection to find the edge
+        edges = cv2.Canny(frame, 50, 150)  # for some reason this halves the image in both width and height
+        # Average edge position over all horizontal slices.
+        if stop is None:
+            stop = int(self.rectAOI.s32Height.value / 2)
+        edge = [np.argmax(edges[i]) for i in range(start, stop)]
+        self.edge_pos.append(np.average(edge))
+        # # ------------------------------------------------------------------------------------------------------------
 
         # ...and finally display it
         cv2.imshow("SimpleLive_Python_uEye_OpenCV", frame)
@@ -232,55 +263,112 @@ class Camera(object):
             print("is_CaptureVideo ERROR")
 
         # Enables the queue mode for existing image memory sequences
-        nRet = ueye.is_InquireImageMem(self.hCam, self.pcImageMemory, self.MemID, self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel, self.pitch)
+        nRet = ueye.is_InquireImageMem(self.hCam, self.pcImageMemory, self.MemID,
+                                       self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel, self.pitch)
         if nRet != ueye.IS_SUCCESS:
             print("is_InquireImageMem ERROR")
-        else:
-            print("Live video activated. Press q to quit")
-            self.live_video_loop(nRet)
-            self.release_memory()
 
-    def live_video_loop(self, nRet):
+    def live_video_loop(self, savepath=None, start=0, stop=None):
+        print("Live video activated. Press q to quit")
+        if savepath is not None:
+            print("Data will be saved on quit")
         # Continuous image display
-        while (nRet == ueye.IS_SUCCESS):
-
-            self.take_image()
+        while True:
+            # ...extract the data of our image memory
+            array = ueye.get_data(self.pcImageMemory, self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel,
+                                  self.pitch, copy=False)
+            # bytes_per_pixel = int(nBitsPerPixel / 8)
+            self.timestamps.append(perf_counter_ns())
+            self.process_image(array, start, stop)
 
             # Press q if you want to end the loop
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                # Destroys the OpenCv windows
+                cv2.destroyAllWindows()
+
+                if savepath is not None:
+                    with open(savepath, 'w') as fp:
+                        fp.write("Timestamp (ms), Edge pixel\n")
+                        t0 = self.timestamps[5]
+                        for t, p in zip(self.timestamps[5:], self.edge_pos[5:]):
+                            fp.write(f"{(t - t0) / 1000000}, {p}\n")
+                    print(f"File saved to {savepath}")
+
                 break
 
-    def triggered_video(self):
+    def triggered_video(self,):
         self.set_external_trigger()
-        ueye.is_CaptureVideo(self.hCam, ueye.int(2000))
 
-        self.queue_mode()
+        # ueye.IS_SEQUENCE_LIST_EMPTY
 
-        # Continuous image display
-        while 0 == ueye.IS_SUCCESS:
-            # ueye.is_Event(self.hCam, UINT
-            # nCommand, void * pParam, UINT
-            # nSizeOfParam)
-            try:
-                self.freeze_video()
-                self.take_image()
-                self.release_memory()
-            except ValueError:
-                continue
+        # self.queue_mode()
+        # might be able to enable messages?
+        MessageBox = ctypes.windll.user32.MessageBoxW
+        hwnd = ctypes.byref(MessageBox)  # MessageBox(None, 'Hello', 'Window title', 0)
+        pyhandle = win32event.CreateEvent(None, False, False, None)
+        # chandle = ctypes.c_long(pyhandle.handle)
+        # print(ueye.is_EnableMessage(self.hCam, ueye.IS_FRAME, hWnd=hwnd))
+        # # print(ueye.is_EnableMessage(self.hCam, ueye.IS_TRIGGER, out))
+        # print('out', chandle.value)
 
-            # Press q if you want to end the loop
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                self.stop_video()
-                break
+        nRet = ueye.is_InitEvent(self.hCam, hwnd, ueye.IS_SET_EVENT_FRAME)
+        print(nRet)
+        nRet = ueye.is_EnableEvent(self.hCam, ueye.IS_SET_EVENT_FRAME)
+        print(nRet)
 
+        nRet = ueye.is_FreezeVideo(self.hCam, ueye.IS_WAIT)
+        print("Freeze", nRet)
+
+        # print('out', chandle.value)
+        while nRet == ueye.IS_SUCCESS:
+            ret = win32event.WaitForSingleObject(hwnd, 2)  # timeout
+            print("ret", ret)
+            # print('out', chandle.value)
+            array = ueye.get_data(self.pcImageMemory, self.rectAOI.s32Width, self.rectAOI.s32Height, self.nBitsPerPixel,
+                                  self.pitch, copy=False)
+            print("Image ready", array)
+            self.process_image(array)
+
+        # params1 = IS_INIT_EVENT(2, False, False)
+        # params2 = IS_INIT_EVENT(10000, True, False)
+        #
+        # params = (IS_INIT_EVENT * 2)(*(params1, params2))
+        # print(params[0], params[1])
+        #
+        # nret = is_Event(self.hCam, E_EVENT_CMD.IS_EVENT_CMD_INIT, ctypes.byref(params), ctypes.sizeof(params))
+        # print("is event", nret)
+        #
+        # nret = is_Event(self.hCam, E_EVENT_CMD.IS_EVENT_CMD_INIT, ctypes.byref(params), ctypes.sizeof(params))
+
+
+        # self.capture_video(wait=False)  # ueye.int(2000)
+        #
+        # # here we really want to use events to know when the next image is available
+        # while 0 == ueye.IS_SUCCESS:
+        #
+        #
+        #     # nCommand, void * pParam, UINT
+        #     # nSizeOfParam)
+        #     try:
+        #         array = self.freeze_video()
+        #         self.process_image(array)
+        #         self.release_memory()
+        #     except ValueError:
+        #         continue
+        #
+        #     # Press q if you want to end the loop
+        #     if cv2.waitKey(1) & 0xFF == ord('q'):
+        #         self.stop_video()
+        #         ueye.is_ClearSequence(self.hCam)
+        #         break
 
     def release_memory(self):
         # Release the image memory that was allocated using is_AllocImageMem() and remove it from the driver management
         ueye.is_FreeImageMem(self.hCam, self.pcImageMemory, self.MemID)
 
     def close_connection(self):
-        # Destroys the OpenCv windows
-        # cv2.destroyAllWindows()
+        self.stop_video()
+        self.release_memory()
         # Disables the hCam camera handle and releases the data structures and memory areas taken up by the uEye camera
         ueye.is_ExitCamera(self.hCam)
         print()
